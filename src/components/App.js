2b704334bda047ee617a3e46c5b823ebda32aee0
Farhan Alam <metalhead901@live.com>
2021-04-11 21:11:11 +0600

    add bootstrap 5, footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 export default function App(props) {
     return (
-        <Fragment>
+        <div className='d-flex flex-column h-100'>
             <div className='glass'>
                 lmao ok
-                </div>
-        </Fragment>
+            </div>
+            <div className="text-center mt-auto footer">
+                Created by <a className='text-r' href="https://github.com/fa-901" rel='noopener noreferrer' target="_blank">fa-901</a>
+            </div>
+        </div>
     )
 }
