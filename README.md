ef8cabff7c57436a8c8b26ccc947a899373db392
Farhan Alam <metalhead901@live.com>
2021-07-26 23:39:19 +0600

    updated style of chart

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # weather-app
  
-A weather application built from scratch using React and bundled with Parcel.
+A weather application built from with React and bundled with Parcel.
 
----
 ## APIs
 - Weather data retrieved from Open Weather API. https://openweathermap.org/ 
 - City information and list retrieved frm GeoDB Cities API. http://geodb-cities-api.wirefreethought.com/
-
----
 
 ## Features
 - Display weather data of given location (obviously)
 - Weekly Weather Forecasting
 - Text-based city suggestion
-- Glassmorphism design
+- Temperature unit conversion
+- Hourly graphical representation of different attributes of weather data
 
 ## Steps to run
 - Generate an API key from https://openweathermap.org/
--- src/components/DailyDisplay/HourlyDisplay.js
+++ src/components/DailyDisplay/HourlyDisplay.js
@@ -35,7 +35,7 @@ export default function HourlyDisplay(props) {
     const graphData = dataList.filter((e) => {
         return dayjs(e.dt * 1000).isBetween(startTime, endTime, 'minute', '[]')
     }).map((e) => {
-        const time = dayjs.tz((e.dt * 1000), weatherData.timezone).format('D MMM, h A');
+        const time = dayjs.tz((e.dt * 1000), weatherData.timezone).format('h A');
         const temp = tempConversion((isCurrentDay ? e.temp : e.main.temp), unit)
         const value = temp;
         return {
@@ -51,13 +51,14 @@ export default function HourlyDisplay(props) {
     return (
         <div className='hourly mb-3'>
             <div className="label">
-                Hourly Forecast
+                Hourly Forecast, {startTime.format('D MMM')}
             </div>
             <div className="chart-area">
                 <ResponsiveContainer width="100%" height="100%">
                     <ComposedChart data={graphData}>
                         <CartesianGrid strokeDasharray="5 5" />
                         <XAxis
+                            padding={{ left: 15, right: 15 }}
                             dataKey="time"
                             label={{ value: 'Time', offset: -5, position: 'insideBottom' }}
                         />
@@ -66,6 +67,7 @@ export default function HourlyDisplay(props) {
                         />
                         <Tooltip
                             cursor={{ fill: 'transparent' }}
+                            content={<CustomTooltip/>}
                         />
                         <Line
                             type="monotone"
@@ -75,6 +77,20 @@ export default function HourlyDisplay(props) {
                         />
                     </ComposedChart>
                 </ResponsiveContainer>
+            </div>
+        </div>
+    )
+}
+
+const CustomTooltip = ({payload}) => {
+    const value = payload[0]?.payload || {};
+    return (
+        <div className='glass p-2'>
+            <div>
+                {value.time}
+            </div>
+            <div>
+                {value.value}
             </div>
         </div>
     )
